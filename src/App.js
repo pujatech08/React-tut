8de0cbca7c6e291c72b1300ea6e2e50033442152
pujatech08 <chaudharypuja0806@gmail.com>
2026-04-23 11:12:18 +0530

    class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Users from './Users';
 import User from './User';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <h1>React tut</h1>
       <User />
+      <Users />
     </div>
   );
 }
---
+++ src/Users.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class Users extends Component{
+    render(){
+        return(
+            <>
+            <h1>This is a class component</h1>
+            </>
+        )
+    }
+}
+
+export default Users;
